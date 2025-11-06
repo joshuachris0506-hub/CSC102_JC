d6ed3842e6edebd3b3506c59da4961512d802510
joshuachris0506-hub <joshuachris0506@gmail.com>
2025-11-06 13:23:55 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -120,3 +120,45 @@ function playCraps(){
             function getRandNum(){
                 return Math.floor(Math.random() * 800);
             }
+
+            function testPalin(){
+                //get the value that the user entered in the textbox
+                let userInput = document.getElementById("txtInput").value.trim();
+
+                console.log("userInput=" + userInput);
+
+                // move this to a if/else statement
+                isPalindrome(userInput);
+
+                //this statement implies == true, you could write it out
+                // isPalindrome(userInput) == true
+                if (isPalindrome(userInput)){
+                    document.getElementById("divResult").textContent = "Yes, " + userInput + " is a palindrome."
+                }
+                else{
+                    document.getElementById("divResult").textContent = "No, " + userInput + " is not a palindrome."
+                }
+                
+        }
+
+        function isPalindrome(wordToTest){
+            // remove any spaces inside of the word(s)
+            let cleanedWord = wordToTest.replace(/\s/g, "");
+
+            // change any UPPER case letters to lower case
+            cleanedWord = cleanedWord.toLowerCase();
+
+            console.log("cleanedWord=" + cleanedWord);
+            // convert the cleaned string to an array
+            let arrCleaned = cleanedWord.split("");
+
+            // reverse the array content 
+            arrCleaned = arrCleaned.reverse();
+
+            // take our reversed array and convert it back to a string
+            let reversedWord = arrCleaned.join("");
+             
+            console.log("reversedWord=" + reversedWord);
+
+            return cleanedWord == reversedWord;
+        }
